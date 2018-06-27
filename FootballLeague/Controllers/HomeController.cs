3370f04bb70def51cfcc4ea8f3e1b018a54bb387
PBosnjak <petar.bosnjak@yahoo.com>
2018-06-27 23:23:18 +0200

    MatchController and View created

--- FootballLeague/Controllers/HomeController.cs
+++ FootballLeague/Controllers/HomeController.cs
@@ -79,13 +79,6 @@ namespace FootballLeague.Controllers
             return View(p);
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ FootballLeague/Controllers/MatchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballLeague.Controllers
+{
+    public class MatchController : Controller
+    {
+        [Route("Match/{id:int}")]
+        public IActionResult Index(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
