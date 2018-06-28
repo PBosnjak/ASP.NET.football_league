5583ec0d0ee721f29160da08bf21e7133048ca47
PBosnjak <petar.bosnjak1@gmail.com>
2018-06-28 11:42:37 +0200

    Match detail view, controller and model added

--- FootballLeague/Controllers/MatchController.cs
+++ FootballLeague/Controllers/MatchController.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FootballLeague.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FootballLeague.Controllers
 {
     public class MatchController : Controller
     {
+        private readonly DataContext _db;
+
+        public MatchController(DataContext db)
+        {
+            _db = db;
+        }
+
         [Route("Match/{id:int}")]
         public IActionResult Index(int id)
         {
-            ViewData["id"] = id;
-            return View();
+            CardGoalViewModel CGVM = new CardGoalViewModel();
+            CGVM.Goals = _db.Goals
+                .Where(g => g.MatchId == id)
+                .OrderBy(g => g.Minute)
+                .Include(g => g.Player)
+                .ToList();
+            
+            CGVM.Cards = _db.Cards
+                .Where(c => c.MatchId == id)
+                .OrderBy(c => c.Minute)
+                .Include(c => c.Player)
+                .ToList();
+
+            CGVM.Match = _db.Matches
+                .Include(m => m.AwayTeam)
+                .Include(m => m.HomeTeam)
+                .Include(m => m.Referee)
+                .Single(m => m.Id == id);
+
+            return View(CGVM);
         }
     }
 }
---
+++ FootballLeague/Models/CardGoalViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeague.Models
+{
+    public class CardGoalViewModel
+    {
+        public IEnumerable<GoalModel> Goals { get; set; }
+        public IEnumerable<CardModel> Cards { get; set; }
+        public MatchModel Match { get; set; }
+    }
+}
